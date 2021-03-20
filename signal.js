31917b224f87955762ad01ed949a2659672015d1
Robin Vold <robvold98@gmail.com>
2021-03-20 16:51:16 +0100

    gs

--- signal.js
+++ signal.js
@@ -1,4 +1,3 @@
-const { Console } = require('console');
 const express = require('express');
 const http = require('http');
 const app = express();
@@ -11,16 +10,18 @@ const io = require('socket.io')(server,{
 const port = 8000;
 
 const peers = {};
+const rooms = {};
+
+
 
 
 io.on("connection", (socket) => {
     //console.log("on connection: " + socket.id);
-    socket.on("my_name_is", (name)=>{
+    socket.on("my_name_is", () =>{
         if(!peers[socket.id]){
-            peers[socket.id] = name;
+            peers[socket.id] = socket.id;
         }
         socket.emit("you",socket.id);
-        io.sockets.emit("peers",peers);
         
     });
     
@@ -29,9 +30,44 @@ io.on("connection", (socket) => {
     });
 
     socket.on("candidate", (message) => {
-        console.log(message)
         io.to(message.id).emit("candidate", message);
     });
+
+    socket.on("createRoom", (room) => {
+        if(!rooms[room.id]){
+            rooms[room.id] = [socket.id];
+            io.sockets.emit("rooms",rooms);
+            socket.emit("roomCreationSuccess");
+        }else{
+            socket.emit("roomCreationError");
+        }
+    });
+
+    socket.on("viewRooms", () => {
+        socket.emit("rooms", rooms);
+    });
+
+    socket.on("joinRoom", (room) => {
+        socket.emit("joinedRoom", rooms[room.id])
+        rooms[room.id].push(socket.id);
+    
+    });
+
+    socket.on("scout", (roomId, callbackFunc) => {
+        callbackFunc(roomId in rooms);
+    });
+
+    socket.on("leaveRoom", (room) => {
+        rooms[room.id].splice(rooms[room.id].indexOf(socket.id),1);
+        if(rooms[room.id].length === 0){
+            delete rooms[room.id];
+        }else{
+            rooms[room.id].forEach(socketId => {
+                io.to(socketId).emit("thisPeerLeft", socket.id);
+            });
+        }
+    });
+
 
     socket.on("answer", message => {
         io.to(message.id).emit("answer", message);
@@ -39,7 +75,7 @@ io.on("connection", (socket) => {
     
     socket.on("disconnect", () => {
         delete peers[socket.id];
-        io.sockets.emit("peers",peers);
+       // io.sockets.emit("peers",peers);
         
     });
 });

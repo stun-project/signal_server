2df2b46e8d28dfbf331e984c2175e8d6abc62145
Sigmund Granaas <sigmundsgranaas@gmail.com>
2021-03-24 14:31:08 +0100

    * Removing room when they are empty * Signaling to peers when yopu leave a room * only remove clients from rooms it disconnected from

--- signal.js
+++ signal.js
@@ -47,13 +47,15 @@ io.on("connection", (socket) => {
 
   socket.on("joinRoom", (room) => {
     socket.emit("joinedRoom", rooms[room]);
-    console.log(`Attempting to join room, here are the peers: ${rooms[room]}`);
+    console.log(
+      `Attempting to join room${room}, here are the peers: ${rooms[room]}`
+    );
     if (rooms[room]) {
       rooms[room].push(socket.id);
-      console.log(`I, (${socket.id}) Join the room: ${rooms[room]}`);
+      console.log(`I, (${socket.id}) Join the room: ${room}`);
     } else {
       rooms[room] = [socket.id];
-      console.log(`I, (${socket.id}) Created the room: ${rooms[room]}`);
+      console.log(`I, (${socket.id}) Created the room: ${room}`);
       io.sockets.emit("rooms", rooms);
     }
   });
@@ -75,25 +77,46 @@ io.on("connection", (socket) => {
     // }
   });
 
-  socket.on("disconnect", () => {
-    console.log(`somebody disconnected, ${socket.id}`);
+  socket.on("disconnect", (event) => {
+    console.log(event);
+    const disconnecting_peer = socket.id;
+    console.log(
+      `somebody disconnected, ${disconnecting_peer}, proceeding to remove from room`
+    );
+
     for (const room in rooms) {
-      rooms[room].splice(rooms[room].indexOf(socket.id), 1);
-      if (rooms[room].length === 0) {
+      // let x = rooms[room].splice(rooms[room].indexOf(socket.id), 1);
+      console.log(`Checking: ${room}, content: ${rooms[room]}`);
+      console.log(rooms[room]);
+      if (
+        rooms[room] == [] ||
+        !rooms[room] ||
+        rooms[room] === null ||
+        rooms[room] === undefined ||
+        rooms[room] == ""
+      ) {
         console.log(
           `Room:${room} deleted because it was empty: ${rooms[room]}`
         );
         delete rooms[room];
-      } else {
+      } else if (rooms[room].includes(disconnecting_peer)) {
+        console.log(`Removing: ${disconnecting_peer} from room: ${room}`);
+        const disconnecting_peer_index = rooms[room].indexOf(
+          disconnecting_peer
+        );
+        rooms[room].splice(disconnecting_peer_index, 1);
+
+        console.log(
+          `Room: ${room}, now contains: ${rooms[room]}, transmitting leave message to all remaining peers`
+        );
+
+        delete peers[disconnecting_peer];
+
         rooms[room].forEach((socketId) => {
-          console.log(
-            `Room:${room} is not empty, transmitting leaving message to all peers: ${rooms[room]}`
-          );
-          io.to(socketId).emit("thisPeerLeft", socket.id);
+          io.to(socketId).emit("thisPeerLeft", disconnecting_peer);
         });
       }
     }
-    delete peers[socket.id];
   });
 });
 
